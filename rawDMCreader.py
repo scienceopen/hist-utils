37ce864900e13c131842a49c032f7a6a2857f019
scienceopen <scienceopen@users.noreply.github.com>
2014-05-20 04:36:22 -0400

    argparse, video

--- rawDMCreader.py
+++ rawDMCreader.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python3
 # reads .DMCdata files and displays them
 # Michael Hirsch
 # GPL v3+ license
 import os,sys
 import numpy as np
 import matplotlib.pyplot as plt
-#import matplotlib.animation as anim
+import matplotlib.animation as anim
 import matplotlib.cm as cm
+import argparse
 import struct
-#import matplotlib.image as mpimg
-import cv2 #for python3 broken plt.imshow
-#import warnings
+import warnings
 ### local imports
 import getRawInd as gri
 
@@ -17,57 +17,49 @@ import getRawInd as gri
 # Examples:
 # python3 rawDMCreader.py '~/HSTdata/DataField/2013-04-14/HST1/2013-04-14T07-00-CamSer7196_frames_363000-1-369200.DMCdata' 512 512 1 1 'all' 0.01 100 4000
 
-def main(argv):
-    debug=False
-     #sys.argv is always strings
-    nargin = len(argv)
-    BigFN = os.path.expanduser(argv[1])
-    
-    if nargin<3: xPix=512; yPix=512
-    else: xPix = int(argv[2]); yPix = int(argv[3])
-    if nargin<5: xBin = 1; yBin = 1
-    else: xBin= int(argv[4]); yBin= int(argv[5])
-    if nargin<7: FrameInd=[0,0]
-    else: FrameInd =np.arange(int(argv[6]), int(argv[7]))
-    if nargin<9: playMovie=0.01
-    else: playMovie= float(argv[8])
-    if nargin<10: Clim=None
-    else: Clim= (float(argv[9]),float(argv[10]))
-    
-    if nargin<12: rawFrameRate=None
-    else: rawFrameRate= float(argv[11])
-    if (nargin < 13) or rawFrameRate: startUTC=None
-    else: startUTC= argv[12]
-# setup data parameters
-    (SuperX,SuperY,Nmetadata,BytesPerFrame,PixelsPerImage,nFrame,nFrameExtract) = getDMCparam(BigFN,xPix,yPix,xBin,yBin,FrameInd)
+def main(BigFN,xyPix,xyBin,FrameInd,playMovie,Clim,rawFrameRate,startUTC):
+       
+    # setup data parameters
+    SuperX,SuperY,Nmetadata,BytesPerFrame,PixelsPerImage,nFrame,nFrameExtract,FrameInd = getDMCparam(BigFN,xyPix,xyBin,FrameInd)
 
 # preallocate
     data = np.zeros((SuperY,SuperX,nFrameExtract),dtype=np.uint16)
-    rawFrameInd = np.zeros((nFrameExtract,1)) #NOTE python may not require float64 like Matlab..?
+    rawFrameInd = np.zeros((nFrameExtract,1)) 
 
-    fid = open(BigFN, 'rb')
+    with open(BigFN, 'rb') as fid:
+        jFrm=0 
+        for iFrm in FrameInd:
+            data[:,:,jFrm],rawFrameInd[jFrm] = getDMCframe(fid,iFrm,BytesPerFrame,PixelsPerImage,Nmetadata,SuperX,SuperY)
+            jFrm += 1
 
-    jFrm=0 
-    for iFrm in FrameInd:
-        (data[:,:,jFrm],rawFrameInd[jFrm]) = getDMCframe(fid,iFrm,BytesPerFrame,PixelsPerImage,Nmetadata,SuperX,SuperY)
-        jFrm += 1 #has to be here due to zero indexing 
+ 
+    #doPlayMovie(data,SuperX,SuperY,FrameInd,playMovie,Clim,rawFrameInd)
     
-    fid.close()
-    
-    if not (playMovie == 0):
-        if sys.version_info[0] ==3:
-           # from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as FigureCanvas
-            #from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3 as NavigationToolbar
-            sys.exit('Python 3 currently cannot do imshow correctly with Matlabplotlib 1.3.1')
-            #warnings.warn('Python 3 Matplotlib could not plot this as of Matplotlib ver 1.3.1')
-        doPlayMovie(data,SuperX,SuperY,FrameInd,playMovie,Clim,rawFrameInd)
-
-    #ani = anim.ArtistAnimation(hFig,updatePlot, interval=250, blit=True, repeat_delay=1000)
-    #plt.show()
+    if playMovie:
+        print(playMovie)
+        hf = plt.figure(1)
+        himg = plt.imshow(data[:,:,0])
+        ht = plt.title('')
+        #blit=False so that TItle updates!
+        anim.FuncAnimation(hf,animate,range(nFrameExtract),fargs=(data,himg,ht), interval=playMovie, blit=False, repeat_delay=1000)
+        plt.show()
 ########## END OF MAIN #######################
-def getDMCparam(BigFN,xPix,yPix,xBin,yBin,FrameInd):
-    SuperX = int(xPix/xBin) #for python 3
-    SuperY = int(yPix/yBin)
+
+def animate(i,data,himg,ht):
+    #print(i)
+    #himg = plt.imshow(data[:,:,i])
+    himg.set_data(data[:,:,i])
+    ht.set_text('RelFrame#' + str(i) )
+    #'RawFrame#: ' + str(rawFrameInd[jFrm]) +
+                
+    plt.draw() #plot won't update without plt.draw()!
+    #plt.show(False) #breaks (won't play)
+    return himg,ht
+
+def getDMCparam(BigFN,xyPix,xyBin,FrameInd):
+    SuperX = int(xyPix[0]/xyBin[0]) #for python 3
+    SuperY = int(xyPix[1]/xyBin[1])
+
     bpp = 16
     nHeadBytes = 4
     Nmetadata = int(nHeadBytes/2) #TODO not true for DMC2data files!
@@ -76,17 +68,19 @@ def getDMCparam(BigFN,xPix,yPix,xBin,yBin,FrameInd):
     BytesPerFrame = BytesPerImage + nHeadBytes
 
 # get file size
-    if not os.path.isfile(BigFN): sys.exit('file does not exist: ' + BigFN)
-    fileSizeBytes = os.path.getsize(BigFN)
+    try: 
+        fileSizeBytes = os.path.getsize(BigFN)
+    except:
+        raise RuntimeError('file does not exist: ' + BigFN)
 
     if fileSizeBytes < BytesPerImage:
-        sys.exit('File size ' + str(fileSizeBytes) + 
+        raise RuntimeError('File size ' + str(fileSizeBytes) + 
                  ' is smaller than a single image frame!')
 
     nFrame = int(fileSizeBytes / BytesPerFrame)
 
     if nFrame%1 != 0:
-        print("Looks like I am not reading this file correctly, with BPF: " + 
+        warnings.warn("Looks like I am not reading this file correctly, with BPF: " + 
               str(BytesPerFrame) )
 
 
@@ -96,23 +90,31 @@ def getDMCparam(BigFN,xPix,yPix,xBin,yBin,FrameInd):
     print("first / last raw frame #'s: " + str(firstRawInd) + " / " + 
           str(lastRawInd))
 
+# setup frame indices
 # if no requested frames were specified, read all frames. Otherwise, just
 # return the requested frames
-    if all(FrameInd == 0): 
-        FrameInd = np.arange(0,nFrame,1,dtype=np.uint64) # has to be numpy for > comparison
+    if FrameInd is None:
+        print('automatically selected all frames in file')
+        FrameInd = np.arange(nFrame,dtype=int) # has to be numpy for > comparison
+    elif isinstance(FrameInd,int):
+        FrameInd =np.arange(FrameInd,dtype=int) 
+    elif len(FrameInd) == 3:
+        FrameInd =np.arange(FrameInd[0],FrameInd[1],FrameInd[2],dtype=int)
+
+
+
     badReqInd = FrameInd>nFrame
 # check if we requested frames beyond what the BigFN contains
-    if any(badReqInd):
-        sys.exit('You have requested Frames ' + str(FrameInd[badReqInd]) +
+    if np.any(badReqInd):
+        raise RuntimeError('You have requested Frames ' + str(FrameInd[badReqInd]) +
                  ', which exceeds the length of ' + BigFN)
-    nFrameExtract = len(FrameInd); #to preallocate properly
+    nFrameExtract = len(FrameInd) #to preallocate properly
 
     nBytesExtract = nFrameExtract*BytesPerFrame
     print('Extracting ' + str(nFrameExtract) + ' frames, totaling ' + str(nBytesExtract) + ' bytes.')
     if nBytesExtract > 2e9:
-        print('This will require ' + str(nBytesExtract/1e9) +
-              ' Gigabytes of RAM. Do you have enough RAM?')
-    return SuperX,SuperY,Nmetadata,BytesPerFrame,PixelsPerImage,nFrame,nFrameExtract
+        warnings.warn('This will require ' + str(nBytesExtract/1e9) + ' Gigabytes of RAM. Do you have enough RAM?')
+    return SuperX,SuperY,Nmetadata,BytesPerFrame,PixelsPerImage,nFrame,nFrameExtract,FrameInd
 
 def getDMCframe(fid,iFrm,BytesPerFrame,PixelsPerImage,Nmetadata,SuperX,SuperY):
     #print(type(iFrm)); print(type(BytesPerFrame)); 
@@ -122,11 +124,7 @@ def getDMCframe(fid,iFrm,BytesPerFrame,PixelsPerImage,Nmetadata,SuperX,SuperY):
     fid.seek(currByte,0) #no return value
 	#read data
     currFrame = np.fromfile(fid, np.uint16,PixelsPerImage).reshape((SuperY,SuperX))
-    #currVect= np.fromfile(fid, np.uint16,PixelsPerImage)
-    #print(str(iFrm) + ' ' + str(currVect.shape) + 
-    #      'index ' + str(jFrm) )
-    #data[:,:,jFrm] = currVect.reshape((SuperY,SuperX)) #helps with debugging
-
+    
     rawFrameInd = getRawFrameInd(fid,Nmetadata)
     return currFrame,rawFrameInd
 
@@ -138,6 +136,8 @@ def getRawFrameInd(fid,Nmetadata):
     return rawFrameInd
 
 def doPlayMovie(data,SuperX,SuperY,FrameInd,playMovie,Clim,rawFrameInd):
+  if playMovie:
+    print('attemping movie playback')
     hf1 = plt.figure(1)
     hAx = hf1.add_subplot(111)   
     if not Clim:
@@ -153,18 +153,42 @@ def doPlayMovie(data,SuperX,SuperY,FrameInd,playMovie,Clim,rawFrameInd):
     
     jFrm = 0
     for iFrm in FrameInd:
-       # print(str(iFrm) + ' ' + str(jFrm))
-        hIm.set_data(data[:,:,jFrm])  #plt.imshow(currFrame)
+        print(str(iFrm) + ' ' + str(jFrm))
+        plt.imshow(data[:,:,jFrm])
+        #hIm.set_data(data[:,:,jFrm])  #
         hT.set_text('RawFrame#: ' + str(rawFrameInd[jFrm]) + 
                 'RelFrame#' + str(iFrm) )
-        plt.draw()
+        plt.show(True)
         plt.pause(playMovie)
         jFrm += 1  #yes, at end of for loop
     plt.close()
+  else:
+    print('skipped movie playback')
     #hFig.canvas.draw()
 
 
 if __name__ == "__main__":
-    #print(sys.argv)
-    sys.exit(main(sys.argv))
+    p = argparse.ArgumentParser(description='Raw .DMCdata file reader')
+    p.add_argument('-i','--in',help='.DMCdata file name and path',required=True)
+    p.add_argument('-p','--pix',help='nx ny  number of x and y pixels respectively',nargs=2,default=(512,512),type=int)
+    p.add_argument('-b','--bin',help='nx ny  number of x and y binning respectively',nargs=2,default=(1,1),type=int)
+    p.add_argument('-f','--frames',help='frame indices of file (not raw)',nargs=3,metavar=('start','stop','stride'),default=None,type=int)
+    p.add_argument('-m','--movie',help='seconds per frame. ',nargs=1,default=[None],type=float)
+    p.add_argument('-c','--clim',help='min max   values of intensity expected (for contrast scaling)',nargs=2,default=None,type=float)
+    p.add_argument('-r','--rate',help='raw frame rate of camera',nargs=1,default=None,type=float)
+    p.add_argument('-s','--startutc',help='utc time of nights recording',nargs=1,default=None)
+    args = vars(p.parse_args())
+
+    BigFN = BigFN = os.path.expanduser(args['in'])
+    xyPix = args['pix']
+    xyBin = args['bin']
+    FrameInd = args['frames']
+    playMovie = args['movie'][0]
+    Clim = args['clim']
+    rawFrameRate = args['rate']
+    startUTC = args['startutc']
+    
+    
+   
+    main(BigFN,xyPix,xyBin,FrameInd,playMovie,Clim,rawFrameRate,startUTC)
     
