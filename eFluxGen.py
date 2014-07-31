8ea366bccafcaf94b306026551281554740623e3
scienceopen <scienceopen@users.noreply.github.com>
2014-07-31 14:00:14 -0400

    improvements to smoothness

--- eFluxGen.py
+++ eFluxGen.py
@@ -3,39 +3,46 @@
 
 from numpy import pi,exp,logspace,sum,where
 from matplotlib.pyplot import xlabel,ylabel,title,loglog,figure,clf,grid,gca
+from findnearest import find_nearest
 
-E0 = 2500
+E0 = 2500.
 Wb=0.15*E0
-Q0 = 8e11
+Q0 = 8.0e11
 
-E = logspace(2,5,num=200,base=10)
-iE0 = where()
+E = logspace(2,5,num=4000,base=10)
+isimE0,simE0 = find_nearest(E,E0)
 
 # Gaussian base
-base = (Q0/(pi**(3/2)*Wb*E0)) * exp(-((E-E0)/Wb)**2)
-
-# high energy tail
-#Bh = 4.1e3
-bh = 2.9
-het = Bh*(E/E0)**(-bh)
-het[E<E0] = 0
+Qc = Q0/(pi**(3/2)*Wb*E0)
+base = Qc * exp(-((E-E0)/Wb)**2)
 
 #low energy tail
 # for LET, 1<b<2
-Bl = 8200
-bl = 1
+Bl = 8200.
+bl = 1.
 let = Bl*(E/E0)**(-bl)
-let[E>=E0] = 0
+let[E>E0] = 0.
 
 # "third tail"
 B3 = 1.8e4
-b3 = 3
+b3 = 3.
 mid = B3*(E/E0)**(b3)
-mid[E>=E0] = 0
+mid[E>E0] = 0.
 #%%
-arc = base + het + let + mid
+arc = base + let + mid
+# now add high energy tail
+# strickland 1993 said 0.2, but 0.145 gives better match to peak flux
+Bh = 0.145*arc[isimE0] #4100.
+print(Bh)
+bh = 2.9
+het = Bh*(E/E0)**(-bh)
+het[E<E0] = 0.
+
+arc += het
+
 Q = sum(arc)
 print('total flux Q=' + str(Q))
+print('E0 flux =' + str(arc[isimE0]))
 #%%
 figure(1);clf()
 loglog(E,het,'k:')
--- findnearest.py
+++ findnearest.py
@@ -1,7 +1,14 @@
-from numpy import zeros,abs,argmin
+from numpy import zeros,abs,argmin,atleast_1d
 
 def find_nearest(x,x0):
-    #idead based on http://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
+    # inputs:
+    # x: array within which to search for x0
+    # x0: singleton or array of values to search for in x
+    # outputs:
+    # idx: index of x nearest to x0
+    # xidx: x[idx]
+    x0 = atleast_1d(x0)
+    #idea based on http://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
     idx = zeros(len(x0),dtype=int)
 
     for i,xi in enumerate(x0):
