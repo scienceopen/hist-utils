91aa71e82d191cfb9c2542e6a9d37817793e706c
scienceopen <scienceopen@user.noreply.github.com>
2014-09-12 02:03:53 -0400

    cleanup

--- getRawInd.py
+++ getRawInd.py
@@ -9,21 +9,20 @@ def getRawInd(BigFN,BytesPerImage,nHeadBytes,Nmetadata):
     #handle ~ in filename
     BigFN = expanduser(BigFN)
     # gets first and last raw indices from a big .DMCdata file
-    fid=open(BigFN,'rb')
+    with open(BigFN,'rb') as fid:
 
-    fid.seek( int(BytesPerImage) ,0) # get first raw frame index
-    firstRawIndex = meta2rawInd(fid,Nmetadata)
+        fid.seek( int(BytesPerImage) ,0) # get first raw frame index
+        firstRawIndex = meta2rawInd(fid,Nmetadata)
 
-    fid.seek(-nHeadBytes,2) #get last raw frame index
-    lastRawIndex = meta2rawInd(fid,Nmetadata)
+        fid.seek(-nHeadBytes,2) #get last raw frame index
+        lastRawIndex = meta2rawInd(fid,Nmetadata)
 
-    fid.close()
     return firstRawIndex, lastRawIndex
 
 
 def meta2rawInd(fid,Nmetadata):
     #TODO works for .DMCdata only, not bigger header files!
-    metad = fromfile(fid, uint16,Nmetadata)
+    metad = fromfile(fid, dtype=uint16, count=Nmetadata)
     metad = pack('<2H',metad[1],metad[0]) # reorder 2 uint16
-    rawInd = unpack('<I',metad)[0]
+    rawInd = unpack('<I',metad)[0] #always a tuple
     return rawInd
