6ec31adbf1e305bd9a642a4f48376bdf19632a0d
scienceopen <scienceopen@users.noreply.github.com>
2014-11-14 15:42:19 -0500

    cleanup

--- plotSolarElev.py
+++ plotSolarElev.py
@@ -14,36 +14,33 @@ from dateutil.relativedelta import relativedelta
 from airMass import airmass
 
 def main(site,coord,year):
-
-    if site: # one weird trick
-        SiteNames = {
-        "Sondrestrom": Sondrestrom,
-        "PFISR": PFISR,
-        "BU": BU,
-        "Svalbard": Svalbard}
-        lat, lon, elv = SiteNames.get(site)()
+    obs=ephem.Observer()
+    if site == "Sondrestrom":
+        obs.lat='66.98';obs.lon='-50.94';obs.elevation=180
+    elif site=="PFISR":
+        obs.lat='65.12';obs.lon='-147.49';obs.elevation=210
+    elif site=="BU":
+        obs.lat='42.4'; obs.lon='-71.1';obs.elevation=5
+    elif site=="Svalbard":
+        obs.lat='78.23'; obs.lon='15.4';obs.elevation=450
     elif coord[0] is not None:
-        lat,lon,elv = coord[0],coord[1],coord[2]
+        obs.lat,obs.lon = coord[0],coord[1]
     else:
         exit('you must specifiy a site or coordinates')
 
-    obs=ephem.Observer()
     obs.pressure = 1010 # millibar FLOAT
     obs.temp = 15 # deg. Celcius FLOAT
     #obs.horizon = '0' #HAS TO BE A STRING!
-    obs.lat = str(lat) #STRING
-    obs.lon= str(lon) #STRING
-    obs.elevation = float(elv) # meters
 
     sun = ephem.Sun()
 
     dates = pd.date_range(start=year+'-01-01', end=year+'-12-31', freq='D')
 
     sunaltdate,t = elofday(dates,obs,sun)
-    plotyear(dates,t,sunaltdate,site,lat,lon)
+    plotyear(dates,t,sunaltdate,site,obs)
 
     Irr = airmass(sunaltdate)[0]
-    plotIrr(dates,t,Irr,site,lat,lon)
+    plotIrr(dates,t,Irr,site,obs)
 
     show()
 
@@ -67,24 +64,26 @@ def elminutes(times,obs,sun):
         sunalt[i] = sun.alt
     return degrees(sunalt)
 
-def plotyear(dates,t,sunaltdate,site,lat,lon):
+def plotyear(dates,t,sunaltdate,site,obs):
     fg = figure(figsize=(12,7),dpi=110)
     ax = fg.gca()
     V = (-18,-12,-6,-3,0,10,20,30,40,50,60,70,80,90)
     CS = ax.contour(dates,t,sunaltdate,V)
     ax.clabel(CS, inline=1, fontsize=10,fmt='%0.0f')#, manual=manual_locations)
     ax.set_ylabel('UTC')
-    ax.set_title(''.join(('Solar elevation angle (deg.)  ',site,': ',lat,', ',lon)))
+    ax.set_title(''.join(('Solar elevation angle (deg.)  ',site,': ',
+                          str(obs.lat),', ',str(obs.lon))))
     ax.grid(True)
     fg.autofmt_xdate()
 
-def plotIrr(dates,t,sunaltdate,site,lat,lon):
+def plotIrr(dates,t,sunaltdate,site,obs):
     fg = figure(figsize=(12,7),dpi=110)
     ax = fg.gca()
     CS = ax.contour(dates,t,sunaltdate)
     ax.clabel(CS, inline=1, fontsize=10,fmt='%0.0f')#, manual=manual_locations)
     ax.set_ylabel('UTC')
-    ax.set_title(''.join(('Sea level solar irradiance [W/m$^2$] at ',site,': ',lat,', ',lon)))
+    ax.set_title(''.join(('Sea level solar irradiance [W/m$^2$] at ',site,': ',
+                          str(obs.lat),', ',str(obs.lon))))
     ax.grid(True)
     fg.autofmt_xdate()
     show()
@@ -97,27 +96,12 @@ def plotday(t,sunalt,site):
     ax.grid(True)
     ax.set_title(site + ' ' + t[0].strftime('%Y-%m-%d'))
 
-class Site:
-    def __init__(self,lat,lon,elv):
-        #we're going to let pyephem do error checking
-        self.lat=lat
-        self.lon=lon
-        self.elv=elv
-def Sondrestrom():
-    return '66.98','-50.94', 180
-def PFISR():
-    return '65.12', '-147.49', 210
-def BU():
-    return '42.4', '-71.1', 5
-def Svalbard():
-    return '78.23', '15.4', 450
-
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='plots solar elevation angle')
     p.add_argument('-s','--site',help='use a prestored site [sondrestrom, pfisr, bu, svalbard]',type=str,default='')
-    p.add_argument('-c','--coord',help='specify site lat lon [degrees] alt [meters]',
-                   nargs=3,type=str,default=[None, None, None])
+    p.add_argument('-c','--coord',help='specify site lat lon [degrees] ',
+                   nargs=2,type=str,default=(None, None))
     p.add_argument('-y','--year',help='year to plot',type=str,default='2015')
     ar = p.parse_args()
     main(ar.site,ar.coord,ar.year)
