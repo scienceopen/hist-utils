1a2184bfdb5e3a0f286da9b77a82f89a5e39a8ed
scivision <scivision@users.noreply.github.com>
2014-11-13 19:22:38 -0500

    Update airMass.py

--- airMass.py
+++ airMass.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-# Michael Hirsch
-# following http://www.pveducation.org/pvcdrom/properties-of-sunlight/air-mass
-# assumes observer at sea level, altitude h \approx 0
+"""
+Michael Hirsch
+following http://www.pveducation.org/pvcdrom/properties-of-sunlight/air-mass
+assumes observer at sea level, altitude h \approx 0
+"""
 from numpy import sin,radians,arange
-from matplotlib.pyplot import figure,gca,show
+from matplotlib.pyplot import figure,show
 
-thetaE = arange(91,dtype=float)  #solar elevation angle above horizon
+thetaE = arange(90+1,dtype=float)  #solar elevation angle above horizon
 
 M = ( sin(radians(thetaE)) + 0.50572*(6.07995+thetaE)**(-1.6364) )**-1 #air mass factor
 
-figure(1)
-ax=gca()
+ax=figure().gca()
 ax.plot(thetaE,M)
 ax.set_xlabel('Solar Elevation Angle  [deg.]')
 ax.set_ylabel('Air Mass Factor')
@@ -21,8 +22,7 @@ I0 = 1353 # [W]
 
 I = I0 * 0.7**M**0.678
 
-figure(2)
-ax=gca()
+ax=figure().gca()
 ax.plot(thetaE,I)
 ax.set_title('Solar Irradiance at sea level vs. Solar Elevation Angle')
 ax.set_xlabel('Solar Elevation Angle  [deg.]')
