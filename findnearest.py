ad96153dbb58f86f13c48f0f025d626d1daa4d6d
Michael <hirsch617@gmail.com>
2015-03-01 15:43:07 -0500

    docs

--- findnearest.py
+++ findnearest.py
@@ -1,22 +1,28 @@
-'''
+"""
+inputs:
+x: array within which to search for x0
+x0: singleton or array of values to search for in x
+
+outputs:
+idx: index of x nearest to x0
+xidx: x[idx]
+
 Note: You should consider using Python's bisect command instead of this function.
 I made this function for people coming from Matlab who didn't know about bisect yet.
 If you need the index and not just the value, consider the 'sortedcontainers' package
 http://grantjenks.com/docs/sortedcontainers/
 This find_nearest function does NOT assume sorted input
-'''
+
+idea based on http://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
+
+Michael Hirsch
+GPLv3+
+"""
 from numpy import empty_like,absolute,atleast_1d,asanyarray
 
 def find_nearest(x,x0):
     x = asanyarray(x) #for indexing upon return
-    # inputs:
-    # x: array within which to search for x0
-    # x0: singleton or array of values to search for in x
-    # outputs:
-    # idx: index of x nearest to x0
-    # xidx: x[idx]
     x0 = atleast_1d(x0)
-    #idea based on http://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
     idx = empty_like(x0,dtype=int)
 
     for i,xi in enumerate(x0):
