f4aec6e7b43e92d062b05090fd6370d30ad06a52
Michael <hirsch617@gmail.com>
2015-03-01 17:31:46 -0500

    using astropy

--- plotSolarElev.py
+++ plotSolarElev.py
@@ -5,17 +5,19 @@ tested in Python 3.4 and Python 2.7
 Michael Hirsch
  Aug 2012
 """
-import ephem  #pip install ephem
-import pandas as pd
+import astropy.units as u
+from astropy.time import Time, TimeDelta
+from astropy.coordinates import get_sun, EarthLocation, AltAz
 from matplotlib.pyplot import figure,show
-from numpy import degrees,empty_like,empty
-from dateutil.relativedelta import relativedelta
+from numpy import arange
 #
 from airMass import airmass
 
-def main(site,coord,year):
-    obs=ephem.Observer()
-    if site == "Sondrestrom":
+def main(site,coord,year,plotperhour,doplot):
+    site = site.lower()
+    if len(site) == 0 and coord[0] is not None:
+        obs = EarthLocation(lat=coord[0]*u.deg, lon=coord[1]*u.deg, height=coord[2]*u.m)
+    elif site == "Sondrestrom":
         obs.lat='66.98';obs.lon='-50.94';obs.elevation=180
     elif site=="PFISR":
         obs.lat='65.12';obs.lon='-147.49';obs.elevation=210
@@ -23,67 +25,49 @@ def main(site,coord,year):
         obs.lat='42.4'; obs.lon='-71.1';obs.elevation=5
     elif site=="Svalbard":
         obs.lat='78.23'; obs.lon='15.4';obs.elevation=450
-    elif coord[0] is not None:
-        obs.lat,obs.lon = coord[0],coord[1]
     else:
         exit('you must specifiy a site or coordinates')
 
-    obs.pressure = 1010 # millibar FLOAT
-    obs.temp = 15 # deg. Celcius FLOAT
-    #obs.horizon = '0' #HAS TO BE A STRING!
+    plotperday = 24*plotperhour
+    dt = TimeDelta(3600/plotperhour, format='sec')
+    times = Time(year+'-01-01T00:00:00',format='isot',scale='utc') + dt * range(365*plotperday)
+    dates = times[::plotperday].datetime
+    hoursofday = times[:plotperday].datetime
 
-    sun = ephem.Sun()
+    #yes, we need to feed times to observer and sun!
+    sun = get_sun(times).transform_to(AltAz(obstime=times,location=obs))
+    sunel = sun.alt.degree.reshape((plotperday,-1),order='F')
 
-    dates = pd.date_range(start=year+'-01-01', end=year+'-12-31', freq='D')
+    Irr = airmass(sunel)[0]
 
-    sunaltdate,t = elofday(dates,obs,sun)
-    plotyear(dates,t,sunaltdate,site,obs)
+    if doplot:
+        plotIrr(dates,hoursofday,Irr,site,obs)
+        plotyear(dates,hoursofday,sunel,site,obs)
 
-    Irr = airmass(sunaltdate)[0]
-    plotIrr(dates,t,Irr,site,obs)
+        show()
 
-    show()
+    return Irr,sunel
 
-def elofday(dates,obs,sun):
-    """ compute solar elevation for each day of year"""
-    ploteachmin = 15
-    sunaltdate=empty((24*60/ploteachmin+1,dates.size),dtype=float)
-    t0 = dates[0]
-    for j,d in enumerate(dates):
-        t1 = t0 + relativedelta(days=1)
-        times = pd.date_range(start=t0, end=t1, freq=str(ploteachmin)+'T') #T is minute
-        sunaltdate[:,j] = elminutes(times,obs,sun)
-        t0 = t1
-    return sunaltdate,times
-
-def elminutes(times,obs,sun):
-    sunalt = empty_like(times,dtype=float) #need dtype here
-    for i,t in enumerate(times):
-        obs.date = t
-        sun.compute(obs)
-        sunalt[i] = sun.alt
-    return degrees(sunalt)
-
-def plotyear(dates,t,sunaltdate,site,obs):
+def plotyear(dates,hoursofday,sunel,site,obs):
     fg = figure(figsize=(12,7),dpi=110)
     ax = fg.gca()
     V = (-18,-12,-6,-3,0,10,20,30,40,50,60,70,80,90)
-    CS = ax.contour(dates,t,sunaltdate,V)
+    CS = ax.contour(dates,hoursofday,sunel,V)
     ax.clabel(CS, inline=1, fontsize=10,fmt='%0.0f')#, manual=manual_locations)
     ax.set_ylabel('UTC')
     ax.set_title(''.join(('Solar elevation angle (deg.)  ',site,': ',
-                          str(obs.lat),', ',str(obs.lon))))
+                          str(obs.latitude),', ',str(obs.longitude))))
     ax.grid(True)
     fg.autofmt_xdate()
 
-def plotIrr(dates,t,sunaltdate,site,obs):
+def plotIrr(dates,hoursofday,sunel,site,obs):
     fg = figure(figsize=(12,7),dpi=110)
     ax = fg.gca()
-    CS = ax.contour(dates,t,sunaltdate)
+    CS = ax.contour(dates,hoursofday,sunel)
     ax.clabel(CS, inline=1, fontsize=10,fmt='%0.0f')#, manual=manual_locations)
     ax.set_ylabel('UTC')
     ax.set_title(''.join(('Sea level solar irradiance [W/m$^2$] at ',site,': ',
-                          str(obs.lat),', ',str(obs.lon))))
+                          str(obs.latitude),', ',str(obs.longitude))))
     ax.grid(True)
     fg.autofmt_xdate()
     show()
@@ -101,7 +85,9 @@ if __name__ == '__main__':
     p = ArgumentParser(description='plots solar elevation angle')
     p.add_argument('-s','--site',help='use a prestored site [sondrestrom, pfisr, bu, svalbard]',type=str,default='')
     p.add_argument('-c','--coord',help='specify site lat lon [degrees] ',
-                   nargs=2,type=str,default=(None, None))
+                   nargs=3,type=float,default=(None, None, None))
     p.add_argument('-y','--year',help='year to plot',type=str,default='2015')
+    p.add_argument('--pph',help='plot steps per hour (default 1)',type=int,default=1)
+    p.add_argument('--noplot',help='disable plotting',action='store_false')
     ar = p.parse_args()
-    main(ar.site,ar.coord,ar.year)
+    Irr, sunel = main(ar.site,ar.coord,ar.year,ar.pph,ar.noplot)
