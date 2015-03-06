14a4a945d29f6d6d828e0bef56f05899cf2b6cfa
nucl <hirsch617@gmail.com>
2015-03-06 17:30:49 -0500

    docs

--- walktree.py
+++ walktree.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 """
 Recursively loops for pattern-matching files, like GNU find
 Michael Hirsch
 Dec 2014
 """
-from os import walk
+from os import walk, getcwd #not getcwdu, that's python 2 only
 from os.path import join,expanduser,isdir, isfile
 from fnmatch import filter
 #from stat import S_ISDIR, S_ISREG
@@ -29,8 +30,10 @@ def walktree(root,pat):
 
 
 if __name__ == '__main__':
-    from sys import argv
-    if len(argv) != 3:
-        exit('USAGE: walktree.py rootdir pattern')
-    found = walktree(argv[1],argv[2])
+    from argparse import ArgumentParser
+    p = ArgumentParser(description='Recursively loops for pattern-matching files, like GNU find')
+    p.add_argument('rootdir',help='path including and below which to search',type=str,default=getcwd(),nargs='?')
+    p.add_argument('pattern',help='text to search for (use double apostrophes and globbing e.g. "myfile*" ',type=str)
+    a=p.parse_args()
+    found = walktree(a.rootdir,a.pattern)
     print(found)
