d5dfe7a92c9d3561da17ab49c9840858ad14f097
Michael H <hirsch617@gmail.com>
2015-04-18 20:59:34 -0600

    work with single file input

--- walktree.py
+++ walktree.py
@@ -7,11 +7,15 @@ Dec 2014
 from os import walk, getcwd #not getcwdu, that's python 2 only
 from os.path import join,expanduser,isdir, isfile
 from fnmatch import filter
+from six import string_types
 #from stat import S_ISDIR, S_ISREG
 
 def walktree(root,pat):
+#%% make list if it's a string
+    if isinstance(root,string_types):
+        root = [root]
+#%%
     found = []
-
     for r in root:
         r = expanduser(r)
         if isdir(r):
@@ -34,7 +38,7 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='Recursively loops for pattern-matching files, like GNU find')
     p.add_argument('rootdir',help='path including and below which to search',type=str,default=getcwd(),nargs='?')
-    p.add_argument('pattern',help='text to search for (use double apostrophes and globbing e.g. "myfile*" ',type=str)
+    p.add_argument('pattern',help='text to search for (use double apostrophes and globbing e.g. "myfile*" ',type=str,default="*",nargs='?')
     a=p.parse_args()
 
     found = walktree(a.rootdir,a.pattern)
