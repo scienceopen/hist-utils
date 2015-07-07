fe4327571b99ca8d95b711d926d946aeadf2f5de
scivision <scivision@users.noreply.github.com>
2015-07-06 22:44:01 -0400

    selfTest

---
+++ test_self.m
@@ -0,0 +1,13 @@
+## selftest of functions
+## this is made for GNU Octave only, NOT Matlab !
+
+%!function [testframe, testind] = test_rawread()
+%!  bigfn='test/testframes.DMCdata';
+%!  framestoplay=2;  %this is (start,stop,step) so (1,2,1) means read only the second frame in the file
+%!  [testframe, testind, ~] = rawDMCreader(bigfn,512,512,1,1,framestoplay);
+%!endfunction
+%!
+%!test
+%! [testframe, testind] = test_rawread();
+%! assert(testind,int64(710731))
+%! assert(testframe(1:5,1),uint16([642; 1321;  935;  980; 1114]))
