9025e52c3c180ed1f57badb02c8b9e458b2e4bdf
scienceopen <hirsch617@gmail.com>
2015-07-30 16:29:30 -0400

    case insensitive, pass out image shaped like orig image

--- readDASCfits.py
+++ readDASCfits.py
@@ -3,49 +3,54 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
+from warnings import warn
 #
 from walktree import walktree
 
 def readCalFITS(indir,azfn,elfn):
-    flist = walktree(indir)
-    az,el = readazelfits(azfn,elfn)
-    img = readFITS(flist,az,el)
+    flist = walktree(indir,"*.fits")
+    data,coordnames,dataloc,sensorloc,times = readFITS(flist,azfn,elfn)
 
-    return img,az,el
-    
-def readazelfits(azfn,elfn):
-    """
-    reads one-time spatial calibration (plate scale) of DASC all-sky camera.
-    """
-    
+    return data['image']  
 
-def readFITS(flist,azmap,elmap):
+def readFITS(flist,azfn,elfn):
+    """
+    reads FITS images and spatial az/el calibration for allsky camera
+    """
     
-    header = fits.open(flist[0])
+    header = fits.open(flist[0],mode='readonly')
     img = header[0].data
-    data = np.zeros((img.size,len(flist)))
-    dataloc = np.zeros((img.size,3))  
-    times = np.zeros((len(flist),2))
+    #data = np.empty((img.size,len(flist)))
+    dataloc = np.empty((img.size,3))  
+    times =   np.empty((len(flist),2))
     for i in range(len(flist)):
         try:
             fn = flist[i]
             fund = fn.find('PKR')
-            date = (datetime(int(fn[fund+14:fund+18]),int(fn[fund+18:fund+20]),int(fn[fund+20:fund+22]),int(fn[fund+23:fund+25]),int(fn[fund+25:fund+27]),int(fn[fund+27:fund+29]))-datetime(1970,1,1,0,0,0)).total_seconds()
+            date = (datetime(int(fn[fund+14:fund+18]),
+                             int(fn[fund+18:fund+20]),
+                             int(fn[fund+20:fund+22]),
+                             int(fn[fund+23:fund+25]),
+                             int(fn[fund+25:fund+27]),
+                             int(fn[fund+27:fund+29]))-datetime(1970,1,1,0,0,0)).total_seconds()
             times[i] = [date,date+1]
             header = fits.open(fn)
             img = header[0].data
-            data[:,i] = img.flatten()
-        except:
-            print fn,'has error'
-    data = {'image':data}    
+            #data[:,i] = img.flatten() #
+        except Exception as e:
+            print(fn+ 'has error {}'.format(e))
+    data = {'image':img}    
         
     coordnames="spherical"
-    
-    az = fits.open(azmap)[0].data
-    el = fits.open(elmap)[0].data
-    dataloc[:,0] = 135 # in km, this is an assumtion, john take note
-    dataloc[:,1] = az.flatten()
-    dataloc[:,2] = el.flatten()
+    try:
+        az = fits.open(azfn,mode='readonly')[0].data
+        el = fits.open(elfn,mode='readonly')[0].data
+        dataloc[:,0] = 135 #NOTE in km, this is an assumtion, john take note
+        dataloc[:,1] = az.ravel()
+        dataloc[:,2] = el.ravel()
+    except Exception as e:
+        warn('could not read az/el mapping.   {}'.format(e))
+        dataloc=None
     
     sensorloc=np.array([65,-148,0])
         
@@ -55,10 +60,10 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='for Poker Flat DASC all sky camera, read az/el mapping and images')
     p.add_argument('indir',help='directory of .fits or specific .fits file')
-    p.add_argument('azfn',help='filename for DASC .fits azimuth calibration')
-    p.add_argument('elfn',help='filename for DASC .fits elevation calibration')  
+    p.add_argument('azfn',help='filename for DASC .fits azimuth calibration',nargs='?')
+    p.add_argument('elfn',help='filename for DASC .fits elevation calibration',nargs='?')  
     p=p.parse_args()
 
 
-    img,az,el = readFITS(p.indir,p.azfn,p.elfn)
+    img = readCalFITS(p.indir,p.azfn,p.elfn)
     
--- walktree.py
+++ walktree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 Recursively loops for pattern-matching files, like GNU find
+this function is case-insensitive (see line 59)
 Michael Hirsch
 Dec 2014
 """
@@ -8,6 +9,7 @@ from os import walk, getcwd #not getcwdu, that's python 2 only
 from os.path import join,expanduser,isdir, isfile
 from fnmatch import filter
 from six import string_types
+from warnings import warn
 #from stat import S_ISDIR, S_ISREG
 
 def walktree(root,pat):
@@ -20,7 +22,7 @@ def walktree(root,pat):
         r = expanduser(r)
         if isdir(r):
             for top,dirs,files in walk(r):
-                for f in filter(files,pat):
+                for f in filter((ff.lower() for ff in files),pat.lower()):
                     found.append(join(top,f))
 
             if len(found)==0:
@@ -28,8 +30,7 @@ def walktree(root,pat):
         elif isfile(r):
             found.append(r)
         else:
-            print("***walktree: is " + r + " a file or directory?")
-            #found.append(None)
+            warn("is {} a file or directory?".format(r))
 
     return found
 
