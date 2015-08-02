83de1719885b9977f86aa564cd36192941f2078e
scivision <scivision@users.noreply.github.com>
2015-08-02 02:49:52 -0400

    move

--- histutils/__init__.py
+++ histutils/__init__.py
@@ -1,3 +1,3 @@
-from . import (airMass,cp_parents,empty_file,findstars,getRawInd,
+from . import (airMass,cp_parents,empty_file,findnearest,findstars,getRawInd,
 	h5lister,hstxmlparse,normframe,plotSolarElev,rawDMCreader,readDASCfits,
 	sixteen2eight,walktree)
