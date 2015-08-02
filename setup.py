139bb3e8607e8ead7bf3ec9b60e34f844dda4228
scivision <scivision@users.noreply.github.com>
2015-08-02 02:23:50 -0400

    prereq

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='histutils',
 	  author='Michael Hirsch',
 	  author_email='hirsch617@gmail.com',
 	  url='https://github.com/scienceopen/histutils',
-	  install_requires=['scipy','scikit-image','astropy','matplotlib','six','nose'],
+	  install_requires=['scipy','scikit-image','h5py','astropy','matplotlib','six','nose'],
       packages=['histutils'],
 	  )
 
