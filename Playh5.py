506096f266f93451c4c4b28a3055aa9565659b57
scivision <scivision@users.noreply.github.com>
2015-08-27 02:06:30 -0400

    driver

--- Playh5.py
+++ Playh5.py
@@ -1,18 +1,16 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 """
 Plays video contained in HDF5 file, especially from rawDMCreader program.
 """
 from __future__ import division,absolute_import
 import h5py
-try:
-    from .rawDMCreader import doPlayMovie
-except:
-    from rawDMCreader import doPlayMovie
+#
+from histutils.rawDMCreader import doPlayMovie
 
-def playh5movie(h5fn):
+def playh5movie(h5fn,imgh5):
 
     with h5py.File(h5fn,'r',libver='latest') as f:
-        data = f['/rawimg']
+        data = f[imgh5]
         try:
             ut1_unix = f['/ut1_unix']
         except:
@@ -22,7 +20,8 @@ def playh5movie(h5fn):
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='play hdf5 video')
-    p.add_argument('h5fn',help='hdf5 .h5 file with video data in path /imgdata')
+    p.add_argument('h5fn',help='hdf5 .h5 file with video data')
+    p.add_argument('imgh5',help='path / variable inside hdf5 file to image stack (default=rawimg)',default='rawimg')
     p = p.parse_args()
 
-    playh5movie(p.h5fn)
+    playh5movie(p.h5fn,p.imgh5)
