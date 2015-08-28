6cfeb5ee6970a15be8d53b5ccce7e9507143b96d
scivision <scivision@users.noreply.github.com>
2015-08-28 17:33:14 -0400

    user configuration clim

--- Playh5.py
+++ Playh5.py
@@ -11,8 +11,9 @@ from numpy import dstack
 #from histutils.sixteen2eight import sixteen2eight
 from histutils.rawDMCreader import doPlayMovie
 
-def playh5movie(h5fn,imgh5,outfn):
+def playh5movie(h5fn,imgh5,outfn,clim):
     h5fn = expanduser(h5fn)
+
 
     with h5py.File(h5fn,'r',libver='latest') as f:
         data = f[imgh5]
@@ -22,11 +23,13 @@ def playh5movie(h5fn,imgh5,outfn):
             ut1_unix = None
 
         if outfn:
-            hdf2video(data,h5fn,imgh5,outfn)
+            hdf2video(data,imgh5,outfn,clim)
         else:
-            doPlayMovie(data,0.1,ut1_unix=ut1_unix)
+            doPlayMovie(data,0.1,ut1_unix=ut1_unix,clim=clim)
 
-def hdf2video(data,h5fn,imgh5,outfn):
+def hdf2video(data,imgh5,outfn,clim):
+    outfn = expanduser(outfn)
+
     import cv2
     try:
         from cv2.cv import FOURCC as fourcc #Windows needs from cv2.cv
@@ -37,7 +40,9 @@ def hdf2video(data,h5fn,imgh5,outfn):
     outfn = stem + '.ogv'
     cc4 = fourcc(*'THEO')
     # we use isColor=True because some codecs have trouble with grayscale
-    hv = cv2.VideoWriter(outfn,cc4, fps=36, frameSize=data.shape[1:], isColor=True) #right now we're only using grayscale
+    hv = cv2.VideoWriter(outfn,cc4, fps=33,
+                         frameSize=data.shape[1:][::-1],  #frameSize needs col,row
+                         isColor=True) #right now we're only using grayscale
     if not hv.isOpened():
         raise TypeError('trouble starting video file')
 
@@ -45,7 +50,7 @@ def hdf2video(data,h5fn,imgh5,outfn):
         #RAM usage explodes if scaling all at once on GB class file
     #for d in bytescale(data,1000,4000):
     #for d in sixteen2eight(data,(1000,4000)):
-        hv.write(gray2rgb(bytescale(d,1000,4000)))
+        hv.write(gray2rgb(bytescale(d,clim[0],clim[1])))
 
     hv.release()
 
@@ -58,6 +63,7 @@ if __name__ == '__main__':
     p.add_argument('h5fn',help='hdf5 .h5 file with video data')
     p.add_argument('-p','--imgh5',help='path / variable inside hdf5 file to image stack (default=rawimg)',default='rawimg')
     p.add_argument('-o','--output',help='output new video file instead of playing back')
+    p.add_argument('-c','--clim',help='contrast limits used to convert 16-bit to 8-bit video',nargs=2,type=float)
     p = p.parse_args()
 
-    playh5movie(p.h5fn,p.imgh5,p.output)
+    playh5movie(p.h5fn,p.imgh5,p.output,p.clim)
