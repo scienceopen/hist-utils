64b501fb20e4ef9629c55348e849f8165e380398
scivision <scivision@users.noreply.github.com>
2015-11-10 01:17:38 -0500

    pathlib

--- histutils/walktree.py
+++ histutils/walktree.py
@@ -4,35 +4,45 @@ Recursively loops for pattern-matching files, like GNU find
 Michael Hirsch
 Dec 2014
 """
-from os import walk
-from os.path import join,expanduser,isdir, isfile
-from fnmatch import filter
-from six import string_types
 from six.moves import getcwd
-from warnings import warn
+from pathlib2 import Path
+#from os import walk
+#from os.path import join,expanduser,isdir, isfile
+#from fnmatch import filter
+#from six import string_types
+#from warnings import warn
 #from stat import S_ISDIR, S_ISREG
 
-def walktree(root,pat):
-#%% make list if it's a string
-    if isinstance(root,string_types):
-        root = [root]
-#%%
-    found = []
-    for r in root:
-        r = expanduser(r)
-        if isdir(r):
-            for top,dirs,files in walk(r):
-                # using .lower makes output lower too. Maybe need to explicitly iterate
-                #for f in filter((ff.lower() for ff in files),pat.lower()):
-                for f in filter(files,pat):
-                    found.append(join(top,f))
-        elif isfile(r):
-            found.append(r)
-        else:
-            warn("is {} a file or directory?".format(r))
+def walktree(root,pat=''):
+    """ with pathlib, this functionality is builtin. Left here as compatibility layer
+    and to raise awareness of pathlib
+
+    output:
+    generator with all recursively globbed paths (use sorted() to get a sorted list. Just list() doesn't work.)
+    """
+    return Path(root).expanduser().glob('**/*'+pat)
 
 
-    return found
+#def walktree_obsolete(root,pat):
+##%% make list if it's a string
+#    if isinstance(root,string_types):
+#        root = [root]
+##%%
+#    found = []
+#    for r in root:
+#        r = expanduser(r)
+#        if isdir(r):
+#            for top,dirs,files in walk(r):
+#                # using .lower makes output lower too. Maybe need to explicitly iterate
+#                #for f in filter((ff.lower() for ff in files),pat.lower()):
+#                for f in filter(files,pat):
+#                    found.append(join(top,f))
+#        elif isfile(r):
+#            found.append(r)
+#        else:
+#            warn("is {} a file or directory?".format(r))
+#
+#    return found
 
 
 if __name__ == '__main__':
