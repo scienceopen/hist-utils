fa6af92ee0b498235244d3e98a4dfccc329ee143
scivision <scivision@users.noreply.github.com>
2015-11-11 22:43:00 -0500

    api

--- histutils/empty_file.py
+++ histutils/empty_file.py
@@ -1,22 +1,33 @@
 """
 empty_file.py
 makes an empty file
-in bash you'd do   >myfile
+in bash you'd do   >myfile or touch myfile
 """
-from six import string_types
+from six import string_types,PY2
 from os import makedirs
-from os.path import dirname
+from pathlib2 import Path
+if PY2: FileNotFoundError=IOError
 
 def empty_file(flist):
-    if isinstance(flist,string_types):
+    if isinstance(flist,(string_types,Path)):
         flist=[flist]
-          
+
     def _blank(f):
-        with open(f,'w'): pass
-            
+        with f.open('w'):
+            pass
+
     for f in flist:
+        f = Path(f)
         try:
             _blank(f)
-        except IOError:
-            makedirs(dirname(f))
+        except FileNotFoundError:
+            makedirs(str(f.parent))
             _blank(f)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('dirs',help='directories to create',nargs='+',default='')
+    p = p.parse_args()
+
+    empty_file(p.dirs)
