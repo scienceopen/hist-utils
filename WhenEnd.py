2330a080ecec2dfd380d46c1f72b8c232b87eb9b
michael <scienceopen@noreply.github.com>
2016-03-01 00:09:10 -0500

    simple frame number to utc time estimate

--- WhenEnd.py
+++ WhenEnd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-calculates what approximate time a .DMCdata file ends, based on inputs:
+calculates a few times relevant to a .DMCdata file depending on inputs:
 starttime
 fps
 xpix,ypix
+and so on
 """
-from __future__ import division
 from datetime import timedelta
 from dateutil.parser import parse
 from argparse import ArgumentParser
@@ -13,14 +13,21 @@ from argparse import ArgumentParser
 p = ArgumentParser(description='calculates what approximate time a .DMCdata file ends, based on inputs')
 p.add_argument('starttime',help='date and time of first frame')
 p.add_argument('fps',help='frames per second',type=float)
-p.add_argument('xypix',help='binned pixel count',nargs=2,type=int)
-p.add_argument('filesize',help='file size in bytes of big .DMCdata file',type=int)
+p.add_argument('-k','--frameind',help='frame indices to give times for',nargs='+',type=int)
+p.add_argument('--xy',help='binned pixel count',nargs=2,type=int)
+p.add_argument('-f','--filesize',help='file size in bytes of big .DMCdata file',type=int)
 p.add_argument('--nheadbytes',help='number of bytes in each frame for header (default 4)',type=int,default=4)
 p=p.parse_args()
 
 tstart = parse(p.starttime)
-nframes = p.filesize//(p.xypix[0] * p.xypix[1] * 2 + 4)
-totalsec = nframes/p.fps
+#%% find start end times
+if p.xy and p.filesize:
+    nframes = p.filesize//(p.xy[0] * p.xy[1] * 2 + 4)
+    totalsec = nframes/p.fps
 
-tend = tstart + timedelta(seconds=totalsec)
-print('tstart {}  tend {}'.format(tstart,tend))
+    tend = tstart + timedelta(seconds=totalsec)
+    print('tstart {}  tend {}'.format(tstart,tend))
+#%% find times corresponding to frame #s
+if p.frameind:
+    for k in p.frameind:
+        print('frame {}:  {}'.format(k,tstart + timedelta(seconds=(k-1)*1/p.fps)))
