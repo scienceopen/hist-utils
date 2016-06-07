433b74457ef940b25cc89f46946157595dec1550
scivision <scivision@users.noreply.github.com>
2016-06-06 20:24:40 -0400

    prereq

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ with open('README.rst','r') as f:
 	long_description = f.read()
 
 setup(name='histutils',
-      version=0.1,
 	  description='utilities for the HiST auroral tomography system',
 	  long_description=long_description,
 	  author='Michael Hirsch',
@@ -25,7 +24,6 @@ setup(name='histutils',
         'https://github.com/scienceopen/pymap3d/tarball/master#egg=pymap3d',
         'https://github.com/scienceopen/dmcutils/tarball/master#egg=dmcutils',
         ],
-      packages=['histutils'],
 	  )
 
 
