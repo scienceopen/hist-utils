f74d13fc7ac362f7ffd2e3afcd78bda49d0f2cc8
scivision <scivision@users.noreply.github.com>
2016-08-02 11:55:06 -0400

    syntax

--- histutils/sixteen2eight.py
+++ histutils/sixteen2eight.py
@@ -1,4 +1,4 @@
-from __future__ import division,absolute_import
+from __future__ import division
 from numpy import uint8,float32
 """
 Note: consider using scipy.misc.bytescale instead of this file.
@@ -16,12 +16,12 @@ def sixteen2eight(I,Clim):
     return Q.round().astype(uint8) # convert to uint8
 
 def normframe(I,Clim):
-"""
-inputs:
--------
-I: 2-D Numpy array of grayscale image data
-Clim: length 2 of tuple or numpy 1-D array specifying lowest and highest expected values in grayscale image
-"""
+    """
+    inputs:
+    -------
+    I: 2-D Numpy array of grayscale image data
+    Clim: length 2 of tuple or numpy 1-D array specifying lowest and highest expected values in grayscale image
+    """
     Vmin = Clim[0]; Vmax = Clim[1]
 
     return (I.astype(float32).clip(Vmin, Vmax) - Vmin) / (Vmax - Vmin) #stretch to [0,1]
