6a4a3936bdec503f843c04f72620c8a12b6cb791
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-30 22:28:37 -0400

    index

--- histutils/__init__.py
+++ histutils/__init__.py
@@ -267,7 +267,7 @@ def vid2h5(data, ut1, rawind, ticks, outfn, P, cmdlog='', i:int=0, Nfile:int=1, 
                     fdt = f.create_dataset('/detect', shape=(N,), dtype=int)
                     fdt.attrs['description'] = '# of auroral detections this frame'
 
-                f['/detect'] = det
+                f['/detect'][ind] = det
 
             if 'params' not in f:
                 cparam = np.array((
