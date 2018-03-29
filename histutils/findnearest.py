1da01020cb7f3d704cfcbc4bbc45cf2006b90175
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-29 18:19:30 -0400

    warn if no overlap FOV

--- histutils/findnearest.py
+++ histutils/findnearest.py
@@ -1,4 +1,5 @@
-from numpy import unravel_index,ma,atleast_1d,empty
+import numpy as np
+import logging
 #
 from pymap3d.haversine import anglesep
 
@@ -14,22 +15,28 @@ def findClosestAzel(az,el,azpts,elpts,discardEdgepix=True):
     assert az.shape    == el.shape
     assert azpts.shape == elpts.shape
 
-    az = ma.masked_invalid(az)
-    el = ma.masked_invalid(el)
+    az = np.ma.masked_invalid(az)
+    el = np.ma.masked_invalid(el)
     nearRow = []; nearCol=[]
     # can be FAR FAR faster than scipy.spatial.distance.cdist()
     for apts,epts in zip(azpts,elpts): #list of arrays or 2-D array
-        apts = atleast_1d(apts); epts = atleast_1d(epts) # needed
+        apts = np.atleast_1d(apts)
+        epts = np.atleast_1d(epts) # needed
+        if np.isnan(apts).all() or np.isnan(epts).all():
+            logging.warning('all points for smaller FOV were outside larger FOV')
+            continue
         assert apts.size==epts.size
-        r = empty(apts.size,dtype=int); c = empty(apts.size,dtype=int)
+        r = np.empty(apts.size,dtype=int)
+        c = np.empty(apts.size,dtype=int)
         for i,(apt,ept) in enumerate(zip(apts,epts)):
             #we do this point by point because we need to know the closest pixel for each point
             errang = anglesep(az,el, apt,ept)
 
+
             """
             THIS UNRAVEL_INDEX MUST BE ORDER = 'C'
             """
-            r[i], c[i] = unravel_index(errang.argmin(), az.shape,order='C')
+            r[i], c[i] = np.unravel_index(errang.argmin(), az.shape,order='C')
 
         if discardEdgepix:
             mask = ((c==0) | (c == az.shape[1]-1) |
@@ -37,7 +44,7 @@ def findClosestAzel(az,el,azpts,elpts,discardEdgepix=True):
         else:
             mask = False
 
-        nearRow.append(ma.array(r,mask=mask))
-        nearCol.append(ma.array(c,mask=mask))
+        nearRow.append(np.ma.array(r,mask=mask))
+        nearCol.append(np.ma.array(c,mask=mask))
 
     return nearRow,nearCol
