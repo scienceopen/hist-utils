f84f7169356fb33f1759aba6460ab83e60a90c66
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-10 03:26:25 -0400

    modernize

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-
-install_requires = ['python-dateutil', 'pytz', 'numpy', 'scipy', 'h5py', 'astropy',
-                    'pymap3d', 'sciencedates']
-tests_require = ['pytest', 'coveralls', 'flake8', 'mypy']
-
-
-setup(name='histutils',
-      packages=find_packages(),
-      author='Michael Hirsch, Ph.D.',
-      url='https://github.com/scivision/histutils',
-      description='Utilities for reading HiST data, etc.',
-      long_description=open('README.md').read(),
-      long_description_content_type="text/markdown",
-      version='0.9.2',
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Scientific/Engineering :: Atmospheric Science',
-          'Programming Language :: Python :: 3.6',
-      ],
-      install_requires=install_requires,
-      python_requires='>=3.6',
-      extras_require={'plot': ['tifffile', 'matplotlib', 'seaborn', ],
-                      'io': ['dascutils', 'themisasi'],
-                      'tests': tests_require},
-      tests_require=tests_require,
-      scripts=['ConvertDMC2h5.py', 'HDDcost.py', 'Playh5.py', 'RunSimulFrame.py',
-               'date2doy.py', 'WhenEnd.py', 'WienerAurora.py'],
-      include_package_data=True,
-      )
+from setuptools import setup
+setup()
