859a91d3894cb57faed34881c6ea16130b90571e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-23 17:52:43 -0400

    update to pytest

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 from pathlib import Path
 from numpy import uint16, int64, array
-from numpy.testing import assert_allclose, assert_array_equal, run_module_suite
-#
+import pytest
+from pytest import approx
 from histutils.rawDMCreader import goRead
 from histutils.diric import diric
 from histutils.findnearest import findClosestAzel
 
-tdir = Path(__file__).parent
+R = Path(__file__).parent
 
 
 def test_diric():
-    assert_allclose(diric(3., 2), 0.0707372)
+    assert diric(3., 2) == approx(0.0707372)
 
 
 def test_rawread():
-    bigfn = tdir / 'testframes.DMCdata'
-    # this is (start,stop,step) so (1,2,1) means read only the second frame in the file
-    framestoplay = (1, 2, 1)
+    bigfn = R / 'testframes.DMCdata'
+    framestoplay = (1, 2, 1)  # this is (start,stop,step) so (1,2,1) means read only the second frame in the file
 
-    testframe, testind, finf = goRead(
-        bigfn, (512, 512), (1, 1), framestoplay, verbose=1)
+    testframe, testind, finf = goRead(bigfn, (512, 512), (1, 1), framestoplay, verbose=1)
 
     # these are both tested by goRead
     # finf = getDMCparam(bigfn,(512,512),(1,1),None,verbose=2)
@@ -31,13 +29,13 @@ def test_rawread():
     assert testind.dtype == int64
     assert testframe.dtype == uint16
     assert testind == 710730
-    assert_array_equal(testframe[0, :5, 0], [956, 700, 1031, 730, 732])
-    assert_array_equal(testframe[0, -5:, -1], [1939, 1981, 1828, 1752, 1966])
+    assert (testframe[0, :5, 0] == [956,  700, 1031,  730,  732]).all()
+    assert (testframe[0, -5:, -1] == [1939, 1981, 1828, 1752, 1966]).all()
 
 
 def test_nearazel():
-    az = array([[3., 4, 5],
-                [2.5, 3.5, 4.5],
+    az = array([[3.,   4,    5],
+                [2.5,  3.5,  4.5],
                 [2.75, 3.75, 4.75]])
     el = array([[1.1, 1.1, 1.1],
                 [1.8, 1.8, 1.8],
@@ -51,4 +49,4 @@ def test_nearazel():
 
 
 if __name__ == '__main__':
-    run_module_suite()
+    pytest.main(['-xrsv', __file__])
