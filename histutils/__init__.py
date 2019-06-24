8de22ccd677ed8533ba7e49278c4a640057ccc5c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-06-24 14:02:04 -0400

    imports for dmcutils

--- histutils/__init__.py
+++ histutils/__init__.py
@@ -3,5 +3,6 @@ need to use int64 for indexing
 since Numpy thru 1.11 defaults to int32 on Windows for dtype=int,
 and we need int64 for large files
 """
-from .utils import splitconf, write_quota  # noqa: F401
+from .utils import splitconf, write_quota, sixteen2eight  # noqa: F401  # dmcutils
 from .index import req2frame, getRawInd, meta2rawInd  # noqa: F401
+from .io import setupimgh5  # noqa: F401   # dmcutils
